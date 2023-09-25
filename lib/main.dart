963478ddeac3c69f236b3dcc43681d79672c15e9
Rodrigo Lima <rodrigolima9657@gmail.com>
2023-09-24 21:16:10 -0300

    add wakelock package to keep the screen awake

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-
+import 'package:wakelock/wakelock.dart';
 import 'src/app_widget.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Wakelock.enable();
   runApp(const MyApp());
 }
